314621a89d4769bd66fbbdd88dd27f2c8bafba3d
kendell <92357397+Kendellb@users.noreply.github.com>
2025-01-24 01:14:34 -0500

    Update .config/polybar/launch.sh

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env sh
-
-# Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-#polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
- killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch polybar
-polybar mybar &
-echo "Bars launched..."
+##!/usr/bin/env sh
+#
+## Terminate already running bar instances
+## If all your bars have ipc enabled, you can use 
+##polybar-msg cmd quit
+## Otherwise you can use the nuclear option:
+# killall -q polybar
+#
+## Wait until the processes have been shut down
+#while pgrep -x polybar >/dev/null; do sleep 1; done
+#
+## Launch polybar
+#polybar mybar &
+#echo "Bars launched..."
