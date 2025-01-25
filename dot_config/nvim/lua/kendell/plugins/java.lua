d7a987d609c516b5d2899c47a49a0cfe1a574e6c
kendell <92357397+Kendellb@users.noreply.github.com>
2025-01-25 17:21:26 -0500

    Update .config/nvim/lua/kendell/plugins/java.lua

--- dot_config/nvim/lua/kendell/plugins/java.lua
+++ dot_config/nvim/lua/kendell/plugins/java.lua
@@ -49,7 +49,7 @@ local config = {
   --
   -- vim.fs.root requires Neovim 0.10.
   -- If you're using an earlier version, use: require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-  root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"})
+  root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"}),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
@@ -70,6 +70,3 @@ local config = {
     bundles = {}
   },
 }
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
